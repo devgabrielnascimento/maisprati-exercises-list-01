429385fe6190de535f353b3b187912f4be7e7f6d
devgabrielnascimento <devgabrielnascimento@gmail.com>
2025-05-19 05:27:12 -0300

    :recycle: refactor: adjust code indentation in ex_09_countdown.js

--- ex_09_countdown.js
+++ ex_09_countdown.js
@@ -1,11 +1,11 @@
 //A simple countdown from 10 to 1
 const { showProgramName } = require("./showProgramName");
+console.log(showProgramName(__filename));
 for (let i = 10; i >= 1; i--) {
   console.log(i);
 }
 const countdown = [];
 let n = 0;
-console.log(showProgramName(__filename));
 console.log("Countdown from 10 to 0 with an array");
 for (let i = 10; i >= 1; i--) {
   countdown[n++] = i;
